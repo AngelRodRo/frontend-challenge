3047463c6d68b2191793186483e0a6298b0bb0c0
Angel <aleangrodriguez123@gmail.com>
2024-02-25 18:20:25 -0500

    feat: add more icons

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -1,8 +1,15 @@
 import { lazy } from 'react';
+
 const PhoneIcon = lazy(() => import('../../assets/icons/GlTelephoneSolid.svg?react'));
 const RimacLogoIcon = lazy(() => import('../../assets/icons/logo.svg?react'));
 const HousePlanIcon = lazy(() => import('../../assets/icons/IcHomeLight.svg?react'));
 const HouseClinicPlanIcon = lazy(() => import('../../assets/icons/IcHospitalLight.svg?react'));
+const ForMePlanIcon = lazy(() => import('../../assets/icons/IcProtectionLight.svg?react'));
+const ForSomeoneelsePlanIcon = lazy(() => import('../../assets/icons/IcAddUserLight.svg?react'));
+const UserIcon = lazy(() => import('../../assets/icons/glFamily.svg?react'));
+const GreenCheckIcon =lazy(() => import('../../assets/icons/green-check.svg?react'));
+
+
 interface Props {
     name: IconName
 }
@@ -10,16 +17,23 @@ interface Props {
 export enum IconName {
     phone = 'phone',
     rimacLogo = 'rimacLogo',
+    forMePlan = 'forMePlan',
+    forSomeoneelsePlan = 'forSomeelsePlan',
     housePlan = 'housePlan',
-    houseClinicPlan = 'houseClinicPlan'
+    user = 'user',
+    houseClinicPlan = 'houseClinicPlan',
+    greenCheck = 'greenCheck'
 }
 
 const iconDictionary: { [k in IconName]: JSX.Element } = {
     [IconName.phone]: <PhoneIcon />,
     [IconName.rimacLogo]: <RimacLogoIcon />,
+    [IconName.forMePlan]: <ForMePlanIcon />,
+    [IconName.forSomeoneelsePlan]: <ForSomeoneelsePlanIcon />,
     [IconName.housePlan]: <HousePlanIcon />,
+    [IconName.user] : <UserIcon />,
     [IconName.houseClinicPlan]: <HouseClinicPlanIcon />,
-    
+    [IconName.greenCheck]: <GreenCheckIcon />
 }
 
 export const Icon: React.FC<Props> = ({ name }) => {
