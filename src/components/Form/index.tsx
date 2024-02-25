a39bc9e82c9033e4b5e94417e987d2d7e3e6f416
Angel <aleangrodriguez123@gmail.com>
2024-02-25 18:22:51 -0500

    feat: add form validation

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,46 +1,59 @@
 import cn from 'classnames';
+import { useContext } from 'react';
+import { useForm } from 'react-hook-form';
 import { useNavigate } from "react-router-dom";
+import { ObjectSchema, boolean, object, string } from "yup";
 
+import { yupResolver } from "@hookform/resolvers/yup";
 import { Button, ButtonVariants } from '../Button';
 import { Input } from '../Input';
 import { InputWithDropdown } from '../InputWithDropdown';
 import { Link } from '../Link';
 
-import { useContext } from 'react';
-import { useForm } from 'react-hook-form';
 import { UserInsuranceContext, type UserInsuranceContextValues } from '../../context/UserInsuranceContext';
 import { User } from '../../models/User';
 import { routes } from '../../router/routes';
 import { Checkbox } from '../Checkbox';
 import styles from './styles.module.scss';
+
 interface Props extends React.FormHTMLAttributes<HTMLFormElement> {
 
 }
 
-interface FormViewModel extends User {
+interface IFormInput extends Pick<User, "phone" | "dni">  {
     privacyPolicy: boolean;
     comercialPolicy: boolean;
 }
+
+const validationSchema: ObjectSchema<IFormInput> = object({
+    phone: string().matches(/^\d{9}$/, {
+        message: 'El número de teléfono no es válido'
+      }).required(),
+    dni: string().matches(/^\d{8}$/, { message: 'El DNI debe tener exactamente 8 dígitos numéricos' }).required(),
+    privacyPolicy: boolean().required("Required"),
+    comercialPolicy: boolean().required("Required")
+});
 
 export const Form: React.FC<Props> = ({ className, ...props }) => {
     const { updateUser } = useContext<UserInsuranceContextValues>(UserInsuranceContext);
     const navigate = useNavigate();
 
-    const form = useForm<FormViewModel>(
+    const form = useForm<IFormInput>(
         {
             defaultValues: {
                 dni: '',
                 phone: '',
                 privacyPolicy: false,
                 comercialPolicy: false
-            }
-        }
+            },
+            resolver: yupResolver(validationSchema)
+        },
     );
 
     const { errors } = form.formState
 
-    const onHandleSubmit = (data: FormViewModel) => {
-        updateUser(data)
+    const onHandleSubmit = (data: IFormInput) => {
+        updateUser({ dni: data.dni, phone: data.phone });
         navigate(routes.Plans);
     }
 
@@ -48,16 +61,16 @@ export const Form: React.FC<Props> = ({ className, ...props }) => {
         <form {...props} onSubmit={form.handleSubmit(onHandleSubmit)} className={cn(styles.Form, className)}>
             <p className={styles.Form__text}>Tú eliges cuánto pagar. Ingresa tus datos, cotiza y recibe nuestra asesoría, 100% online.</p>
             <div className={styles.Form__input}>
-                <InputWithDropdown label="Nro. de Documento" { ...form.register("dni", { required: true }) } />
-                {errors.dni && <span>DNI es requerido</span>}
+                <InputWithDropdown label="Nro. de Documento" { ...form.register("dni") } />
+                {errors.dni && <span>{errors.dni.message}</span>}
             </div>
             <div className={styles.Form__input}>
-                <Input label="Celular" { ...form.register("phone", { required: true }) } />
-                {errors.phone && <span>Celular es requerido</span>}
+                <Input label="Celular" { ...form.register("phone") } />
+                {errors.phone && <span>{errors.phone.message}</span>}
             </div>
             <div className={styles.Form__terms}>
-                <Checkbox className={styles.Form__checkbox} label='Acepto lo Política de Privacidad' { ...form.register("privacyPolicy", { required: true }) }  />
-                <Checkbox className={styles.Form__checkbox} label='Acepto la Política Comunicaciones Comerciales' { ...form.register("comercialPolicy", { required: true }) }/>
+                <Checkbox className={styles.Form__checkbox} label='Acepto lo Política de Privacidad' { ...form.register("privacyPolicy",) }  />
+                <Checkbox className={styles.Form__checkbox} label='Acepto la Política Comunicaciones Comerciales' { ...form.register("comercialPolicy") }/>
                 {errors.comercialPolicy || errors.privacyPolicy ? <span>Acepte los terminos y condiciones</span> : null}
                 <div className={styles.Form__link}>
                     <Link href='#'>Aplican Términos y Condiciones.</Link>
