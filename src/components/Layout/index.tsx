dd4195565ce6d088c32ee0471a21ee12c7849540
Angel <aleangrodriguez123@gmail.com>
2024-02-24 20:12:10 -0500

    feat: add main layout

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,20 @@
+import { Footer } from "../Footer";
+import { Header } from "../Header";
+
+interface Props {
+    children: React.ReactNode
+}
+
+export const Layout: React.FC<Props> = ({  children }) => {
+    return (
+        <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+            <div style={{ flex: 1 }}>
+                <Header />
+                <div>
+                    {children}
+                </div>
+            </div>
+            <Footer />
+        </div>
+    );
+};
