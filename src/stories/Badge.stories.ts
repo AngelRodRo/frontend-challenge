9a8c47892f221fbff95b69bb916b94ae36cc2787
Angel <aleangrodriguez123@gmail.com>
2024-02-24 20:17:20 -0500

    feat: add Badge Storybook story

---
+++ src/stories/Badge.stories.ts
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Badge } from '../components/Badge';
+
+const meta = {
+  title: 'Basics/Badge',
+  component: Badge,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+
+} satisfies Meta<typeof Badge>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    text: 'Hello World'
+  },
+};
