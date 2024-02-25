3bb56a80bcd807c9d48402ed1a4446a4829c21f2
Angel <aleangrodriguez123@gmail.com>
2024-02-24 20:14:51 -0500

    feat: add custom link component

---
+++ src/components/Link/index.tsx
@@ -0,0 +1,11 @@
+import cn from 'classnames';
+import styles from './styles.module.scss';
+
+
+interface Props extends React.AnchorHTMLAttributes<HTMLAnchorElement>  {
+
+}
+
+export const Link: React.FC<Props> = ({  className, ...props }) => {
+    return (<a {...props} className={cn(styles.Link, className)} />);
+};
