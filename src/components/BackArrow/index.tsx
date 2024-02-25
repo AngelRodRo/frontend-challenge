75fd98c31e1dc9481c5c27bf0335c5ff00055446
Angel <aleangrodriguez123@gmail.com>
2024-02-25 18:19:22 -0500

    feat: add backArrow component

---
+++ src/components/BackArrow/index.tsx
@@ -0,0 +1,15 @@
+import cn from 'classnames';
+import styles from './styles.module.scss';
+
+interface Props {
+    onClick?: () => void;
+}
+
+export const BackArrow: React.FC<Props> = ({ onClick }) => {
+    return (<div onClick={onClick} className={styles.backIcon}>
+        <div className={styles.arrowWrap}>
+            <span className={cn(styles.arrowPart, styles.arrowPart1)}></span>
+            <span className={cn(styles.arrowPart, styles.arrowPart2)}></span>
+        </div>
+    </div>);
+};
