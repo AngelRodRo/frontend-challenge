e995895f595aec90903a6094b3c4c6e9fbafe30d
Angel <aleangrodriguez123@gmail.com>
2024-02-24 20:18:10 -0500

    feat: add home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,11 @@
+import { Badge } from "../../components/Badge";
 
+import familyImgSmall from "../../assets/images/family-136w.png";
+import familyImgLarge from "../../assets/images/family-480w.png";
+
+import { useMemo } from "react";
+import { Form } from "../../components/Form";
+import styles from './styles.module.scss';
 
 interface Props {
 
@@ -6,9 +13,38 @@ interface Props {
 
 const HomePage: React.FC<Props> = () => {
 
+    const renderedHomeInfo = useMemo(( ) => {
+        return (
+            <>
+                <Badge text="Seguro Salud Flexible"/>
+                <h1 className={styles.Home__title}>Creado para ti y tu familia</h1>
+            </>
+        )
+    }, []);
+
     return (
         <section className={styles.Home}>
-
+            <div className={styles.Home__heroSection}>
+                <div className={styles.Home__info}>
+                    {renderedHomeInfo}
+                </div>
+                    <img
+                        className={styles.Home__heroImage}
+                        srcSet={`${familyImgSmall} 136w, ${familyImgLarge} 480w`}
+                        src={familyImgLarge}
+                        sizes="
+                        (max-width: 400px) 136px,
+                        480px"
+                        alt="Family"
+                    />
+            </div>
+            <hr className={styles.Home__divider} />
+            <div className={styles.Home__formSection}>
+                <div className={styles.Home__formDescription}>
+                    {renderedHomeInfo}
+                </div>
+                <Form className={styles.Home__form} />
+            </div>
         </section>
     );
 };
---
+++ src/stories/Card.stories.ts
@@ -0,0 +1,30 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Card } from '../components/Card';
+
+const meta = {
+  title: 'Basics/Card',
+  component: Card,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+
+} satisfies Meta<typeof Card>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    children: 'Hello World'
+  },
+};
+
+
+export const Selected: Story = {
+    args: {
+        children: 'Hello World',
+        selected: true
+    },
+  };
