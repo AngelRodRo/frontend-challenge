5261dfb1b3de1ae32f14f0ab2eee563391037303
Angel <aleangrodriguez123@gmail.com>
2024-02-25 20:22:56 -0500

    feat: improve inputWithDropdown

--- src/components/InputWithDropdown/index.tsx
+++ src/components/InputWithDropdown/index.tsx
@@ -1,19 +1,22 @@
 import { forwardRef } from "react";
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
 import { Input } from "../Input";
+
 import styles from "./styles.module.scss";
+
 interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string;
+  options: string[];
 }
 
 export const InputWithDropdown: React.FC<Props> = forwardRef<
   HTMLInputElement,
   Props
->(({ label, ...props }, ref) => {
+>(({ label, options, ...props }, ref) => {
   return (
     <div className={styles.InputWithDropdown}>
-      <select className={styles.InputWithDropdown__dropdown}>
-        <option>DNI</option>
-      </select>
+      <Dropdown controlClassName={styles.InputWithDropdown__dropdown} arrowClassName={styles.InputWithDropdown__dropdownArrow} value={options[0]} options={options} />
       <Input
         {...props}
         label={label}
